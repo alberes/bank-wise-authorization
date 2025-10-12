756d8173b310b5eaff71ab0c8732473a732b79e8
Alberes <alberes.menezes@gmail.com>
2025-10-12 11:23:54 -0300

    Format fileds

--- src/main/java/io/github/alberes/bank/wise/authorization/controllers/dto/TransactionDto.java
+++ src/main/java/io/github/alberes/bank/wise/authorization/controllers/dto/TransactionDto.java
@@ -1,7 +1,13 @@
 package io.github.alberes.bank.wise.authorization.controllers.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record TransactionDto(String type, BigDecimal valor, LocalDateTime data) {
+public record TransactionDto(String type,
+                             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "0.00")
+                             BigDecimal valor,
+                             @JsonFormat(pattern="dd-MM-yyyy HH:mm:ss")
+                             LocalDateTime data) {
 }
--- src/main/java/io/github/alberes/bank/wise/authorization/controllers/dto/TransactionReportDto.java
+++ src/main/java/io/github/alberes/bank/wise/authorization/controllers/dto/TransactionReportDto.java
@@ -1,7 +1,11 @@
 package io.github.alberes.bank.wise.authorization.controllers.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.util.List;
 
-public record TransactionReportDto(BigDecimal saldoTotal, List<TransactionDto> historico) {
+public record TransactionReportDto(
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "0.00")
+        BigDecimal saldoTotal, List<TransactionDto> historico) {
 }
